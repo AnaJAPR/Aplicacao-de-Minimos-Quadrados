36b9a9e8ff892174f350c11252f46f9106fa4021
eduardammag <eduardammag@gmail.com>
2023-11-04 16:55:25 -0300

    c

---
+++ duda_energy.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+
+# Ler os dados do arquivo Excel
+df = pd.read_csv('Sleep_Efficiency_data.csv')
+
+
+
+
