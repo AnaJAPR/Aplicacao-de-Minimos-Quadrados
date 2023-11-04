9e87be99599194b9a7d4970497061d3ccc1bdd9b
eduardammag <eduardammag@gmail.com>
2023-11-04 17:24:32 -0300

    r

--- duda_analise.py
+++ duda_analise.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('Sleep_Efficiency.csv').dropna()
 print(data)
-
+'''
 Sleep_efficiency = data['Sleep efficiency'].values
 Sleep_duration = data['Sleep duration'].values
 
@@ -12,3 +12,4 @@ plt.scatter(Sleep_efficiency, Sleep_duration)
 plt.xlabel('Sleep_efficiency')
 plt.ylabel('Sleep_duration')
 plt.show()
+'''
