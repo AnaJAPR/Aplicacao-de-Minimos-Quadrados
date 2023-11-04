667827d11310b9a1cefe7134d8e441c158e57a5b
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-11-04 14:34:45 -0300

    apenas uma atualização do arquivo

--- analises_ana.py
+++ analises_ana.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 # from sklearn import linear_model
 
-data = pd.read_csv("Sleep_Efficiency.csv").dropna()
+data = pd.read_csv("Sleep_Efficiency.csv")
 
-age = data["age"].values
-sleep_efficiecy = data["Sleep efficiency"].values
+age = data["Age"].values
+sleep_efficiency = data["Sleep efficiency"].values
 
 plt.figure()
-plt.scatter(age, sleep_efficiecy)
+plt.scatter(age, sleep_efficiency)
 plt.xlabel("Age")
 plt.ylabel("Sleep Efficiency")
