4d4eea056d6c75a8b4d7ce13b0d5e151765d7b63
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-11-15 21:29:41 -0300

    testando outra forma de analisar

--- analises_ana.py
+++ analises_ana.py
@@ -1,3 +1,49 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_regression
+from sklearn.linear_model import LinearRegression
+
+# Doing a linear regresion with a random data set
+# x,y = make_regression(n_samples=200, n_features=1, noise=30)
+
+# plt.scatter(x,y)
+
+df = pd.read_csv("Sleep_Efficiency.csv").dropna()
+
+x = df["Sleep duration"].values.reshape(-1,1)
+y = df["Caffeine consumption"].values
+
+# creating a linear regression model and fitting the model to the data
+modelo = LinearRegression()
+modelo.fit(x,y)
+
+# defining coefficients
+a_coeff = modelo.coef_
+l_coeff = modelo.intercept_
+
+print(a_coeff)
+print(l_coeff)
+
+# y = a + b*x
+# y = l_coeff + a_coeff * x
+# print(y)
+
+# defining some characteristics of the graph
+plt.scatter(x,y)
+plt.plot(x, l_coeff + a_coeff*x, color='orange') # line representing linear regression
+plt.scatter(0, l_coeff + a_coeff*2.5, color='lightgreen', s = 20) 
+
+# This shows the graph
+# plt.show()
+
+
+
+
+
+
+
+
+'''
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -37,7 +83,7 @@ least_square_plot = x_hat[0] + x_hat[1] * t_linspace
 MSE = np.mean((least_square - b)**2)
 
 print('MSE: ', MSE)
-'''
+
 regr = linear_model.LinearRegression()
 regr.fit(t.reshape(-1, 1), b)
 
